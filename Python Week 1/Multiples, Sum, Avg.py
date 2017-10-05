d2bd9c0c9001e46b7c7fab741d5de3d710fa9626
Almas Mendygaliyev <31186194+Almazing777@users.noreply.github.com>
2017-10-05 13:21:33 -0400

    Add files via upload

---
+++ Python Week 1/Multiples, Sum, Avg.py
@@ -0,0 +1,20 @@
+# multiples - part I
+
+for count in range (1, 1000, 2):
+    print "looping -", count
+
+# multiples - part II
+
+count = 5
+while count < 1000001:
+    print(count)
+    count *=5
+
+#Sum List
+a = [1,2,5,10,255,3]
+print sum(a)
+
+
+#AverageList
+a = [1,2,5,10,255,3]
+print sum(a)/len(a)
---
+++ Python Week 1/Practice.py
@@ -0,0 +1,92 @@
+import random
+import os
+import sys
+
+# grocery_list = ['juice', 'tomatoes', 'oranges']
+# print 'first item', grocery_list[0]
+#
+# grocery_list[0] = "green juice"
+# print 'first item', grocery_list[0]
+#
+# print grocery_list[1:3]
+#
+# other_events = ['wash car', 'pick up kids', 'cash check']
+#
+# to_do_list = [other_events,grocery_list]
+# print(to_do_list)
+#
+# print((to_do_list[1][1]))
+#
+# grocery_list.append("onions")
+# print(to_do_list)
+#
+# grocery_list.insert(1, 'Pickle')
+#
+# # combine lists together
+# to_do_list2 = other_events + grocery_list
+# print len(to_do_list2)
+# print max(to_do_list2)
+
+# #tuples
+# pi_tumple = (3,1,4,1,5,9)
+#
+# new_tumple = list(pi_tumple)
+# new list = tuple(new_tumple)
+#
+# len(tuple) max(tuple) min(tuple)
+
+# dictionaries
+# super_villains = {'fiddler': 'Isaac Bowin',}
+#
+# print super_villains ['fiddler']
+#
+# del super_villains ['fiddler']
+#
+# #conditionals
+# age = 21
+# #
+# # if age > 16:
+# #     print('You are old enough to drive')
+# # else:
+# #     print('You are not old enough to drive')
+# #
+#
+# # logical operators
+# if ((age >=1) and (age <=18)):
+#     print 'you get a bday'
+# elif (age == 21) or (age >=65):
+#     print 'you get a birthday'
+# elif not (age==30):
+#     print 'you dont get bday'
+# else:
+#     print 'you get a birthday party yeah'
+
+# looping
+# for x in range(0, 10):
+#     print (x)
+
+#while
+# random_num = random.randrange(0,100)
+#
+# while(random_num !=15):
+#     print(random_num)
+#     random_num = random.randrange(0, 100)
+#
+# i = 0;
+#
+# while (i<=20):
+#     if(i%2 == 0):
+#         print(i)
+#     elif (i==9):
+#         break
+#     else:
+#         i = i+ 1
+#         continue
+#     i +=1
+#
+
+def addNumber(fNum , lNum):
+    sumNum - fNum + lNum
+    return sumNum
+
+string = addNumber(1,4)
