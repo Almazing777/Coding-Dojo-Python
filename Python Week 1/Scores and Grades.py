d75968e9314a96d08bf93a1eb43db17e44b11eb2
Almas Mendygaliyev <31186194+Almazing777@users.noreply.github.com>
2017-10-06 13:15:45 -0400

    Scores and Grades Assignment

---
+++ Python Week 1/Scores and Grades.py
@@ -0,0 +1,19 @@
+
+import random
+for x in range (0,100):
+    if x > 90:
+        print [x], "You Grade is an A"
+    elif 80 <= x <= 89:
+        print [x], "Your Grade is B"
+    elif 70 <= x <= 79:
+        print [x], "Your Grade is C"
+    elif 60 <= x <= 69:
+        print [x], "Your Grade is D"
+    else:
+        print [x], "You Grade is F"
+
+print random.randint(1,100)
+
+
+
+
