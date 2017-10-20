3e267fa27f2011b39434573e1004cd8d46a70b0d
Almas Mendygaliyev <31186194+Almazing777@users.noreply.github.com>
2017-10-20 13:47:12 -0400

    end of week 3

---
+++ MySQL Week 3/MySQL Queries/mySQL workbench setup.sql
@@ -0,0 +1,6 @@
+select * from users;
+
+insert into users(first_name, last_name, handle, birthday, created_at, updated_at)
+VALUES ('Almas', 'Mendygaliyev', 'AlmasM', 1989-05-19, now(), now());
+
+DELETE from users where id = 6;
