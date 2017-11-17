e64a96ee3966bec9f1f1a1aacd855eec72126c28
Almas Mendygaliyev <31186194+Almazing777@users.noreply.github.com>
2017-11-16 22:17:03 -0500

    Update models.py

--- PythonDjango - Week 7/courses/main/apps/courses/models.py
+++ PythonDjango - Week 7/courses/main/apps/courses/models.py
@@ -6,9 +6,9 @@ class CourseManager(models.Manager):
 	def basic_validator(self, post_data):
 		errors = []
 		if len(post_data["course_name"]) < 5:
-			errors["course_name"] = "Name field must be 5 characters or more"
+			errors.append("Name field must be 5 characters or more")
 		if len(post_data["desc"]) < 15:
-			errors["desc"] = "Description field must be 15 characters or more"
+			errors.append("Description field must be 15 characters or more")
 		return errors
 
 
